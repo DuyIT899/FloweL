63cf30f738d79bbaf8cd31e0c43ea68252472243
TranMinhHieu <hieutm@imt-soft.com>
2023-06-26 17:29:43 +0700

    first commit

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,54 @@
+<div class=" footer1">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3 col-sm-6 col-12 footer-menu">
+                <div class="footer-logo">
+                    <a class="my-logo" href="#"><i class="flaticon-orchid"></i>Flowra</a>
+                </div>
+                <ul class="list-unstyled">
+                    <li>NewYork, USA</li>
+                    <li>support@example.com</li>
+                    <li>+1 212 147-5241</li>
+                </ul>
+                <ul class="list-unstyled social-media">
+                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                    <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                    <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+                </ul>
+            </div>
+            <div class="col-lg-3 col-sm-6 col-12 footer-menu">
+                <div class="footer-item">
+                    <h4>Quick Links</h4>
+                    <ul class="list-unstyled">
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">About us</a></li>
+                        <li><a href="#">Services</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-3 col-sm-6 col-12 footer-menu">
+                <div class="footer-item">
+                    <h4>Opening Hours</h4>
+                    <ul class="list-unstyled">
+                        <li>Mon – Tue: 10.00 – 18.00</li>
+                        <li>Wed – Thur: 10.00 – 17.00</li>
+                        <li>Fri – Sat: 10.00 – 14.00</li>
+                        <li>Sunday: Closed</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-3 col-sm-6 col-12 footer-menu">
+                <div class="footer-item">
+                    <h4>Newsletters</h4>
+                    <p>Sign up email to get our daily latest news &amp; updates from us</p>
+                    <form class="newsletter-form" action="#" method="post">
+                        <input type="email" class="form-control" name="email" placeholder="Enter your email" required="">
+                        <button class="submit" type="submit" name="send"><i class="fa fa-arrow-right"></i></button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,68 @@
+<nav class="navbar navbar-expand-lg navbar-light header1">
+  <div class="container-fluid">
+    <div>
+      <ul class="navbar-nav ">
+        <li class="nav-item">
+          <a class="nav-link ms-3" aria-current="page" href="#"><i class="fas fa-envelope"></i> email</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link ms-3" href="#"><i class="fas fa-phone"></i> phone</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link ms-3" href="#"><i class="fas fa-map-marker-alt"></i> address</a>
+        </li>
+      </ul>
+    </div>
+    <div>
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link " aria-current="page" href="#"><i class="fab fa-facebook-f"></i></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"><i class="fab fa-instagram"></i></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"><i class="fab fa-youtube-square"></i></a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<nav class="navbar navbar-expand-lg navbar-light header2">
+  <div class="container-fluid ">
+    <div class="col-lg-2 col-12 text-md-center ">
+          <a class="nav-link ms-3 logo" aria-current="page" href="#"><i class="far fa-font-case"></i> logo</a>
+    </div>
+    <div class="col-lg-10 col-12">
+      <ul class="navbar-nav fs-4">
+        <li class="nav-item">
+          <a class="nav-link submenu" aria-current="page" href="#">một chữ</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link submenu" href="#">hai chữ</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link submenu" href="#">ba chữ</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link submenu" aria-current="page" href="#">một chữ</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link submenu" href="#">hai chữ</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link submenu" href="#">ba chữ</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link submenu" aria-current="page" href="#">một chữ</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link submenu" href="#">hai chữ</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link submenu" href="#">ba chữ</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
